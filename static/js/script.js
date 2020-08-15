c582248e25a538b45ca2a89312a6847b566caf20
Abhay Jirati <abhi.jrt12@gmail.com>
2020-08-15 10:58:19 +0530

    completing the challenge 4 : Change the color of all buttons

--- static/js/script.js
+++ static/js/script.js
@@ -113,3 +113,55 @@ function rpsFrontEnd(humanImageChoice, botImagechoice, finalMessage) {
   document.getElementById("flex-box-rps").appendChild(messageDiv);
   document.getElementById("flex-box-rps").appendChild(botDiv);
 }
+
+// Challenge 4 : Change The color of all the Buttons
+var all_buttons = document.getElementsByTagName("button");
+
+var copyAllButtons = [];
+for (let i = 0; i < all_buttons.length; i++) {
+  copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+console.log(copyAllButtons);
+
+function buttonColorChange(buttonThingy) {
+  if (buttonThingy.value === "red") {
+    buttonsRed();
+  } else if (buttonThingy.value === "green") {
+    buttonsGreen();
+  } else if (buttonThingy.value === "reset") {
+    buttonColorReset();
+  } else if (buttonThingy.value === "random") {
+    randomColors();
+  }
+}
+
+function buttonsRed() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add("btn-danger");
+  }
+}
+
+function buttonsGreen() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add("btn-success");
+  }
+}
+
+function buttonColorReset() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add(copyAllButtons[i]);
+  }
+}
+
+function randomColors() {
+  var choices = ["btn-primary", "btn-warning", "btn-danger", "btn-success"];
+  for (let i = 0; i < all_buttons.length; i++) {
+    let randomNumber = Math.floor(Math.random() * 4);
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add(choices[randomNumber]);
+  }
+}
